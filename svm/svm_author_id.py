394513384460162a1c73610d2dc9e0aae03d9ded
Charles Milam <charles.milam@gmail.com>
2016-11-15 22:10:33 -0600

    determine number of predictions attributed to 'Chris'. add variables to hold various results for ease in formatting output.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -28,8 +28,8 @@ import numpy as np
 from sklearn.svm import SVC
 
 # reduce training set size
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 clf = SVC(kernel = 'rbf', C = 10000.0)
 
@@ -41,13 +41,18 @@ print
 
 t1 = time()
 pred = clf.predict(features_test)
+acc = clf.score(features_test, labels_test)
+unique, counts = np.unique(pred, return_counts = True)
+event_counts = dict(zip(unique, counts))
+
 print "prediction time:", round(time()-t1, 3), "s"
 print
 
-print 'Accuracy score:', clf.score(features_test, labels_test)
+print 'Accuracy score:', acc
 print
 print 'Class for 10:', pred[10]
 print 'Class for 26:', pred[26]
 print 'Class for 50:', pred[50]
 print
+print 'Event counts:', event_counts
 #########################################################
