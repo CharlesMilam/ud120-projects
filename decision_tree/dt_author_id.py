aac5150cf0f5dbe3554bff6c00df2eef2e8b8d88
Charles Milam <charles.milam@gmail.com>
2016-11-16 21:19:50 -0600

    initial commit of decision trees. set up classifer, fit and accuracy. modify tools path to work with project structure.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 3 (decision tree) mini-project.
 
-    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Use a Decision Tree to identify emails from the Enron corpus by author:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("./tools/")
 from email_preprocess import preprocess
 
 
@@ -24,8 +24,12 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
 
+clf = DecisionTreeClassifier(random_state = 0, min_samples_split = 40)
+clf.fit(features_train, labels_train)
 
+acc = clf.score(features_test, labels_test)
+
+print 'Accuracy:', acc
 #########################################################
-
-
