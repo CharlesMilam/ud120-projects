0c969128ddacae8829b196d545a1e1d333f03185
Charles Milam <charles.milam@gmail.com>
2016-11-16 22:38:13 -0600

    modify number of estimators, decrease to default of 50.

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -32,7 +32,7 @@ plt.show()
 ### visualization code (prettyPicture) to show you the decision boundary
 from sklearn.ensemble import AdaBoostClassifier
 
-clf = AdaBoostClassifier(n_estimators = 1000)
+clf = AdaBoostClassifier(n_estimators = 50)
 
 clf.fit(features_train, labels_train)
 
