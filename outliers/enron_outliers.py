220c9fe870b28b7c1b32a4a94f5fe9c79d38a36d
Charles Milam <charles.milam@gmail.com>
2016-12-06 21:52:11 -0600

    determine large data point outlier and remove it.

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,6 +10,14 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("./final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+max_bonus = 0
+max_bonus_person = ''
+for key in data_dict:
+    # print key
+    if data_dict[key]['bonus'] > max_bonus and data_dict[key]['bonus'] != 'NaN':
+        max_bonus = data_dict[key]['bonus']
+        max_bonus_person = key
+data_dict.pop(max_bonus_person, 0)
 data = featureFormat(data_dict, features)
 
 
