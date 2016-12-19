9f9aadf7359719e841c509ee780998f71eb3a311
Charles Milam <charles.milam@gmail.com>
2016-12-19 17:14:46 -0600

    modify clustering back to original with 2 features. compare with provided feature scaled plot.

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -69,7 +69,7 @@ feature_1 = "salary"
 feature_2 = "exercised_stock_options"
 feature_3 = 'total_payments'
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -78,7 +78,7 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, _ in finance_features:
+for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
