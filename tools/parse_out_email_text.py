347fd0a617c8c57b38fec375b5e0a2ebc36fa105
Charles Milam <charles.milam@gmail.com>
2016-12-22 21:34:41 -0600

    Initial commit of text_learning. Modify parse_out_email_text for proper project path setup.

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -8,12 +8,12 @@ def parseOutText(f):
         metadata block at the top
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
-        in the email (space-separated) 
-        
+        in the email (space-separated)
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
+
         """
 
 
@@ -33,17 +33,17 @@ def parseOutText(f):
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+
 
 
 
 
     return words
 
-    
+
 
 def main():
-    ff = open("../text_learning/test_email.txt", "r")
+    ff = open("./text_learning/test_email.txt", "r")
     text = parseOutText(ff)
     print text
 
@@ -51,4 +51,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
