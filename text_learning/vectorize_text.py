de26372fedbecb323b99d3aad281d44eeffc5896
Charles Milam <charles.milam@gmail.com>
2016-12-23 00:01:15 -0600

    vectorize the word_data and determine number of words.

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -44,7 +44,7 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('.', path[:-1])
-            print path
+            # print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
@@ -52,7 +52,7 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
             for word in ["sara", "shackleton", "chris", "germani"]:
-                text.replace(word, '')
+                text = text.replace(word, '')
             ### append the text to word_data
             word_data.append(text)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
@@ -77,3 +77,9 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer(stop_words = 'english')
+vectorizer.fit_transform(word_data)
+vector_words = vectorizer.get_feature_names()
+print 'Number of unique words: {}'.format(len(vector_words))
