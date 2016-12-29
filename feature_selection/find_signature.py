ceb6bbe9a065450f3a9c21be7e9b41924f41f428
Charles Milam <charles.milam@gmail.com>
2016-12-29 13:31:39 -0600

    initial commit for feature selection. setup decision tree, determine num of training points, determine accuracy.

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -8,8 +8,8 @@ numpy.random.seed(42)
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
-authors_file = "../text_learning/your_email_authors.pkl"
+words_file = "./your_word_data.pkl"
+authors_file = "./your_email_authors.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
 
@@ -38,6 +38,13 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn.tree import DecisionTreeClassifier
 
+clf = DecisionTreeClassifier()
 
+clf.fit(features_train, labels_train)
 
+score = clf.score(features_test, labels_test)
+
+print 'Score: {}'.format(score)
+print 'Num of training points: {}'.format(len(features_train))
