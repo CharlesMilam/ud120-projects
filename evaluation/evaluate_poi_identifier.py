e2d28a5ca18fe0d58a6300f08921828c56f5dcaa
Charles Milam <charles.milam@gmail.com>
2017-01-12 22:20:38 -0600

    initial commit of metrics. modify paths to match project structure. add in identifier code from previous lesson.

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -13,10 +13,10 @@
 
 import pickle
 import sys
-sys.path.append("../tools/")
+sys.path.append("./tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("./final_project/final_project_dataset.pkl", "r") )
 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
@@ -26,6 +26,16 @@ labels, features = targetFeatureSplit(data)
 
 
 
-### your code goes here 
+### your code goes here
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.3, random_state = 42)
 
+clf = DecisionTreeClassifier()
+
+clf.fit(features_train, labels_train)
+
+score = clf.score(features_test, labels_test)
+
+print 'Score:', score
